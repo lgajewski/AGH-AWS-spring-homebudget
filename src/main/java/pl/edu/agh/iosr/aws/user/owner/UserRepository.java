9d7301d1d9dc612f4bb2af6f564fa08c8579ba9a
Grzegorz Siatka <grzegorz.siatka@spartavity.com>
2017-12-07 22:58:29 +0100

    [users-service] Changes

--- src/main/java/pl/edu/agh/iosr/aws/user/owner/UserRepository.java
+++ src/main/java/pl/edu/agh/iosr/aws/user/owner/UserRepository.java
@@ -5,12 +5,14 @@ package pl.edu.agh.iosr.aws.user.owner;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import pl.edu.agh.iosr.aws.user.model.User;
 
 /**
  * Repository class for <code>Owner</code> domain objects All method names are compliant with Spring Data naming
  * conventions so this interface can easily be extended for Spring Data See here: http://static.springsource.org/spring-data/jpa/docs/current/reference/html/jpa.repositories.html#jpa.query-methods.query-creation
  */
+@CrossOrigin(origins = "*")
 @RepositoryRestResource(collectionResourceRel = "users", path = "users")
 public interface UserRepository extends CrudRepository<User, Integer> {
 
