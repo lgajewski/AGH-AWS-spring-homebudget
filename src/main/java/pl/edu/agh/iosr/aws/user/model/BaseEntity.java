87944ea03309a5b2fc697a283e3e45b9937b049f
Grzegorz Siatka <grzegorz.siatka@spartavity.com>
2017-12-13 20:25:42 +0100

    [stats-service] Changes

--- src/main/java/pl/edu/agh/iosr/aws/user/model/BaseEntity.java
+++ src/main/java/pl/edu/agh/iosr/aws/user/model/BaseEntity.java
@@ -1,21 +1,18 @@
 /*
  * Copyright © 2017 and Confidential to Pegasystems Inc. All rights reserved.
  */
-package pl.edu.agh.iosr.aws.user.model;
-
-import java.io.Serializable;
+package pl.edu.agh.iosr.aws.stats.model;
 
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
+import java.io.Serializable;
 
 /**
  * Simple JavaBean domain object with an id property. Used as a base class for objects
  * needing this property.
  *
- * @author Ken Krebs
- * @author Juergen Hoeller
  */
 @MappedSuperclass
 public class BaseEntity implements Serializable {
