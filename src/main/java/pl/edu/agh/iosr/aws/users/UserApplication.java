e14f56946452852381df2724f9a3e95eb507851d
Lukasz Gajewski <lukasz.gajewski@pega.com>
2017-12-15 17:14:43 +0100

    [users-service] Eureka configuration

--- src/main/java/pl/edu/agh/iosr/aws/users/UserApplication.java
+++ src/main/java/pl/edu/agh/iosr/aws/users/UserApplication.java
@@ -2,9 +2,11 @@ package pl.edu.agh.iosr.aws.users;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class UserApplication {
 
     public static void main(String[] args) throws Exception {
