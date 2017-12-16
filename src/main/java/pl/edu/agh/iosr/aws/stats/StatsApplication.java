72ec7ef0d505be0ebc6556a35e786c313519c53e
Lukasz Gajewski <lukasz.gajewski@pega.com>
2017-12-16 19:24:43 +0100

    [stats-service] Eureka configuration

--- src/main/java/pl/edu/agh/iosr/aws/stats/StatsApplication.java
+++ src/main/java/pl/edu/agh/iosr/aws/stats/StatsApplication.java
@@ -2,8 +2,10 @@ package pl.edu.agh.iosr.aws.stats;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class StatsApplication {
 
     public static void main(String[] args) throws Exception {
