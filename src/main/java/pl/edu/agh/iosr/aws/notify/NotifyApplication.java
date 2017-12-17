1f17abcb7e340be16a94c1a60f0994dd295eae50
Lukasz Gajewski <lukasz.gajewski@pega.com>
2017-12-17 22:41:08 +0100

    [react-service] Temporarily removed eureka from microservices

--- src/main/java/pl/edu/agh/iosr/aws/notify/NotifyApplication.java
+++ src/main/java/pl/edu/agh/iosr/aws/notify/NotifyApplication.java
@@ -2,10 +2,8 @@ package pl.edu.agh.iosr.aws.notify;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient
 public class NotifyApplication {
 
     public static void main(String[] args) throws Exception {
