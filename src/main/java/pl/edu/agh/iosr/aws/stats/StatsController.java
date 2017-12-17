af78412627e4b82c0c0b5cfdadbc5093091073c5
Lukasz Gajewski <lukasz.gajewski@pega.com>
2017-12-17 22:54:09 +0100

    [stats-service] Updated paths for each microservice for health checks

--- src/main/java/pl/edu/agh/iosr/aws/stats/StatsController.java
+++ src/main/java/pl/edu/agh/iosr/aws/stats/StatsController.java
@@ -3,19 +3,21 @@
  */
 package pl.edu.agh.iosr.aws.stats;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-@RequestMapping(value = "/stats")
 class StatsController {
 
     @RequestMapping("/")
     public String home() {
-        return "Hello. This is stats service";
+        return "Hello. This is stats service: /";
+    }
+
+
+    @RequestMapping("/stats-service")
+    public String homeService() {
+        return "Hello. This is stats service: /stats-service";
     }
 }
