4ae8f8587d4f84b78ff0d012a169507d21165abd
Lukasz Gajewski <lukasz.gajewski@pega.com>
2017-12-17 21:18:13 +0100

    [users-service] Temporarily removed eureka from microservices

--- src/main/java/pl/edu/agh/iosr/aws/users/UserApplication.java
+++ src/main/java/pl/edu/agh/iosr/aws/users/UserApplication.java
@@ -2,17 +2,9 @@ package pl.edu.agh.iosr.aws.users;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.rest.core.config.EnumTranslationConfiguration;
-import org.springframework.data.rest.core.config.MetadataConfiguration;
-import org.springframework.data.rest.core.config.ProjectionDefinitionConfiguration;
-import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
-import pl.edu.agh.iosr.aws.users.model.User;
 
 
 @SpringBootApplication
-@EnableDiscoveryClient
 public class UserApplication {
 
     public static void main(String[] args) throws Exception {
