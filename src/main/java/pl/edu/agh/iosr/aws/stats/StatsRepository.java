9dcec24ae2d2ee23cd4e57bee68b2a8bbdbf8f78
Lukasz Gajewski <lukasz.gajewski@pega.com>
2017-12-17 21:24:05 +0100

    [stats-service] Web application: user page

--- src/main/java/pl/edu/agh/iosr/aws/stats/StatsRepository.java
+++ src/main/java/pl/edu/agh/iosr/aws/stats/StatsRepository.java
@@ -5,16 +5,17 @@
 package pl.edu.agh.iosr.aws.stats;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import pl.edu.agh.iosr.aws.stats.model.Entry;
 
-/**
- * Repository class for <code>Owner</code> domain objects All method names are compliant with Spring Data naming
- * conventions so this interface can easily be extended for Spring Data See here: http://static.springsource.org/spring-data/jpa/docs/current/reference/html/jpa.repositories.html#jpa.query-methods.query-creation
- */
+import java.util.List;
+
 @CrossOrigin(origins = "*")
 @RepositoryRestResource(collectionResourceRel = "entries", path = "entries")
 public interface StatsRepository extends CrudRepository<Entry, Integer> {
+
+    List<Entry> findByUser_Id(@Param("userId") Integer id);
 
 }
