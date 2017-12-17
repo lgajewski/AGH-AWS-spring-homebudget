9805a1f9028ea797e674a36168c5cd856842ccb5
Lukasz Gajewski <lukasz.gajewski@pega.com>
2017-12-17 22:40:45 +0100

    Local infrastructure for development

---
+++ spring-homebudget-rest-eureka/src/main/java/pl/edu/agh/iosr/aws/eureka/EurekaApplication.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright © 2017 and Confidential to Pegasystems Inc. All rights reserved.
+ */
+
+package pl.edu.agh.iosr.aws.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+
+}
