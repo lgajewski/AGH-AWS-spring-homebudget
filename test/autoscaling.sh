fac7239c94b1af005a7dae58020206291847952b
Lukasz Gajewski <lukasz.gajewski94@gmail.com>
2017-12-17 23:57:43 +0100

    Added test helpers for triggering autoscaling

---
+++ test/autoscaling.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+URL=${1:-"http://homebudget-cicd-alb-1191825867.eu-west-1.elb.amazonaws.com/"}
+INTERVAL=${2:-5}
+
+while true; do
+    sleep $INTERVAL
+    status_web=`curl -s -o /dev/null -w '%{http_code} OK' $URL`
+    echo "[STATUS] Web microservice: $status_web"
+    sleep 0.3
+    status_stats=`curl -s -o /dev/null -w '%{http_code} OK' $URL'stats-service/entries'`
+    echo "[STATUS] Stats microservice: $status_stats"
+    sleep 0.3
+    status_users=`curl -s -o /dev/null -w '%{http_code} OK' $URL'users-service/users'`
+    echo "[STATUS] Users microservice: $status_users"
+done
