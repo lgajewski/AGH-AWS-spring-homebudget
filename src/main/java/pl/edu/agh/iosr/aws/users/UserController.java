e9561a62d5e9e137b906efdf6b5adcdb37016600
Lukasz Gajewski <lukasz.gajewski@pega.com>
2017-12-17 21:48:05 +0100

    [users-service] Updated paths for each microservice for health checks

--- src/main/java/pl/edu/agh/iosr/aws/users/UserController.java
+++ src/main/java/pl/edu/agh/iosr/aws/users/UserController.java
@@ -1,35 +1,20 @@
 package pl.edu.agh.iosr.aws.users;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.inject.Inject;
-
-
 @RestController
-@RequestMapping(value = "/users")
 class UserController {
 
-    @Value("#{environment['SERVICE_ENDPOINT'] ?: 'localhost:8080'}")
-    private String serviceEndpoint;
-
-    @Inject
-    private UserRepository userRepository;
-
-    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
-
-    @RequestMapping(value = "/")
-    public String index() {
-        return "Hello, this is users microservice";
+    @RequestMapping("/")
+    public String home() {
+        return "Hello. This is users service: /";
     }
 
-    @RequestMapping(value = "/test", method = RequestMethod.GET)
-    public String test() {
-        return "Test endpoint";
+
+    @RequestMapping("/users-service")
+    public String homeService() {
+        return "Hello. This is users service: /users-service";
     }
 
 }
