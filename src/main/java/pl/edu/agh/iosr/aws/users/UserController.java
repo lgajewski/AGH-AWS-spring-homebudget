049171227594937e8165b2cc9a8695044ee4e199
Grzegorz Siatka <grzegorz.siatka@spartavity.com>
2018-01-22 18:44:16 +0100

    [users-service] Add sample log

--- src/main/java/pl/edu/agh/iosr/aws/users/UserController.java
+++ src/main/java/pl/edu/agh/iosr/aws/users/UserController.java
@@ -1,10 +1,14 @@
 package pl.edu.agh.iosr.aws.users;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 class UserController {
+
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
 
     @RequestMapping("/")
     public String home() {
@@ -14,6 +18,7 @@ class UserController {
 
     @RequestMapping("/users-service")
     public String homeService() {
+        logger.info("Hello. This is users service: /users-service");
         return "Hello. This is users service: /users-service";
     }
 
