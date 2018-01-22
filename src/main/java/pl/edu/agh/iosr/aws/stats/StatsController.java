fd2fb221fc8283c052eadaaf75e82fc187bac62c
Grzegorz Siatka <grzegorz.siatka@spartavity.com>
2018-01-22 18:43:46 +0100

    [stats-service] Add sample log

--- src/main/java/pl/edu/agh/iosr/aws/stats/StatsController.java
+++ src/main/java/pl/edu/agh/iosr/aws/stats/StatsController.java
@@ -3,12 +3,16 @@
  */
 package pl.edu.agh.iosr.aws.stats;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
 class StatsController {
+
+    private static final Logger logger = LoggerFactory.getLogger(StatsApplication.class);
 
     @RequestMapping("/")
     public String home() {
@@ -18,6 +22,7 @@ class StatsController {
 
     @RequestMapping("/stats-service")
     public String homeService() {
+        logger.info("Hello. This is stats service: /stats-service");
         return "Hello. This is stats service: /stats-service";
     }
 }
